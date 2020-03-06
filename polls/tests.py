71a7b7b6ef99754b140adce98610f786e2b78e41
Emil Mirmametov <mirmametovez@sibedge.com>
2020-03-06 11:02:47 +0700

    add few tests and fix error with models

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        older_than_day_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=older_than_day_time)
+        self.assertIs(old_question.was_published_recently(), False)
